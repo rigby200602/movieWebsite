790d403c9cc03286ec4dc024db29cfe3fe7c1670
rigby200602 <tungle3456@gmail.com>
2025-04-22 22:51:17 +0700

    a href to fan page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,8 @@ const Navbar = () => {
     return (
         <div className='h-10 text-white'>
             <div className='h-full float-left flex justify-center item-center'>
-                <a className='block h-full text-lg font-medium text-center cursor-pointer hover:bg-white hover:text-black py-1.5 px-1.5'>
+                <a className='block h-full text-lg font-medium text-center cursor-pointer hover:bg-white hover:text-black py-1.5 px-1.5'
+                    href='https://www.facebook.com/fensub20'>
                     Fensub
                 </a>
             </div>
