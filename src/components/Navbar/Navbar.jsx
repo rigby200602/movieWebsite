a15eb909cdb7c93f82b5ba3455a3583d7f2c727e
rigby200602 <tungle3456@gmail.com>
2025-04-22 22:17:35 +0700

    add Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='h-10 text-white'>
+            <div className='float-left flex justify-center content-center py-1.5 px-1.5 hover:bg-white hover:text-black'>
+                <a className='text-lg font-medium text-center cursor-pointer  h-full w-full'>Fensub</a>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
