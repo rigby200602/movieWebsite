30e06e16f59b0c7324c01b82b7e1b1efe6f057b8
rigby200602 <tungle3456@gmail.com>
2025-04-22 22:57:34 +0700

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Navbar = () => {
     return (
         <div className='h-10 text-white'>
-            <div className='h-full float-left flex justify-center item-center'>
+            <div className='h-full float-left flex'>
+                <a className='block h-full text-lg font-medium text-center cursor-pointer hover:bg-white hover:text-black py-1.5 px-1.5'
+                    href='https://www.facebook.com/fensub20'>
+                    Fensub
+                </a>
                 <a className='block h-full text-lg font-medium text-center cursor-pointer hover:bg-white hover:text-black py-1.5 px-1.5'
                     href='https://www.facebook.com/fensub20'>
                     Fensub
