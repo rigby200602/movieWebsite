c81380d4c2c0724968c807c48f341aaded89ce71
rigby200602 <tungle3456@gmail.com>
2025-04-23 18:21:33 +0700

    updating fix navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Navbar from './components/navBar/Navbar'
+import Navbar from './components/navBar/navBar'
 
 function App() {
   return (
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div className='h-12 text-white relative mx-20'>
-                <div className='flex items-center justify-center h-full float-left'>
+        <div className='text-white fixed top-0 left-0 flex justify-between mx-20 w-[90%]'>
+                <div className='flex items-center h-full gap-[20px]'>
                     <a className='h-full text-lg font-medium text-center cursor-pointer hover:bg-white hover:text-black py-1.5 px-1.5'
                         href='https://www.facebook.com/fensub20'>
-                        Fensub
+                        Home
                     </a>
                     <a className='h-full  text-lg font-medium text-center cursor-pointer hover:bg-white hover:text-black py-1.5 px-1.5'
                         href='#'>
-                        Home
+                        Fensub
                     </a>
                 </div>
-                <div className='flex items-center justify-center h-full float-right'>
+                <div className='flex items-center h-full'>
                     <a className='h-full  text-lg font-medium text-center cursor-pointer hover:bg-white hover:text-black py-1.5 px-1.5'
                         href='#'>
                         Danny Phantom
