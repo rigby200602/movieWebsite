3d08631a3bb83bd8bbe72c6c90dd755eb2c28dd6
rigby200602 <tungle3456@gmail.com>
2025-04-23 16:40:19 +0700

    updating style

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,17 +2,27 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div className='h-10 text-white'>
-            <div className='h-full float-left flex'>
-                <a className='block h-full text-lg font-medium text-center cursor-pointer hover:bg-white hover:text-black py-1.5 px-1.5'
-                    href='https://www.facebook.com/fensub20'>
-                    Fensub
-                </a>
-                <a className='block h-full text-lg font-medium text-center cursor-pointer hover:bg-white hover:text-black py-1.5 px-1.5'
-                    href='https://www.facebook.com/fensub20'>
-                    Fensub
-                </a>
-            </div>
+        <div className='h-12 text-white relative mx-20'>
+                <div className='flex items-center justify-center h-full float-left'>
+                    <a className='h-full text-lg font-medium text-center cursor-pointer hover:bg-white hover:text-black py-1.5 px-1.5'
+                        href='https://www.facebook.com/fensub20'>
+                        Fensub
+                    </a>
+                    <a className='h-full  text-lg font-medium text-center cursor-pointer hover:bg-white hover:text-black py-1.5 px-1.5'
+                        href='#'>
+                        Home
+                    </a>
+                </div>
+                <div className='flex items-center justify-center h-full float-right'>
+                    <a className='h-full  text-lg font-medium text-center cursor-pointer hover:bg-white hover:text-black py-1.5 px-1.5'
+                        href='#'>
+                        Danny Phantom
+                    </a>
+                    <a className='h-full  text-lg font-medium text-center cursor-pointer hover:bg-white hover:text-black py-1.5 px-1.5'
+                        href='#'>
+                        Digata Defenders
+                    </a>
+                </div>
         </div>
     )
 }
